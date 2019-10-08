a4b4cf2644264978e502baac0f4681b5bfac1747
conankzhang <conankzhang@gmail.com>
2019-10-08 00:41:41 -0700

    Project: Set up basic platformer controls

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,14 +4,45 @@ using System;
 public class Player : KinematicBody2D
 {
     [Export]
-    public int moveSpeed = 250;
+    private int moveSpeed = 250;
+
+    [Export]
+    private int jumpHeight = -200;
+
+    [Export]
+    private float gravity = 9.8f;
+
+    private Vector2 motion;
+    private Vector2 up;
+
+    public override void _Ready()
+    {
+        motion = new Vector2();
+        up = new Vector2(0, -1);
+    }
 
     public override void _PhysicsProcess(float delta)
     {
-        var motion = new Vector2();
-        motion.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
-        motion.y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
+        motion.y += gravity;
 
-        MoveAndCollide(motion.Normalized() * moveSpeed * delta);
+        if(Input.IsActionPressed("move_right"))
+        {
+            motion.x = moveSpeed;
+        }
+        else if(Input.IsActionPressed("move_left"))
+        {
+            motion.x = -moveSpeed;
+        }
+        else
+        {
+            motion.x = 0;
+        }
+
+        if(IsOnFloor() && Input.IsActionJustPressed("move_up"))
+        {
+            motion.y = jumpHeight;
+        }
+
+        motion = MoveAndSlide(motion, up);
     }
 }
